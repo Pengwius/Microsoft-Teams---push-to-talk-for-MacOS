0c8f03ee92c99ea11618ceab98f1824fcc73db7f
Pengwius <tomekftomekf@gmail.com>
2021-02-18 12:00:36 +0100

    Fix

--- push-to-talk.py
+++ push-to-talk.py
@@ -1,33 +1,41 @@
 import pyautogui
+import time
 from pynput import keyboard
 
 def on_press(key):
     print(key)
-    try: 
-        if key.char == ('p'):
-            print("dupa")
-            pyautogui.keyDown('command')
-            pyautogui.keyDown('shift')
-            pyautogui.keyDown('m')
-            pyautogui.keyUp('command')
-            pyautogui.keyUp('shift')
-            pyautogui.keyUp('m')
+    key_pressed = None
+    try:
+        key_pressed = key.char
     except:
         print("No key")
 
+    if key_pressed == ('p'):
+        pyautogui.keyDown('command')
+        pyautogui.keyDown('shift')
+        pyautogui.keyDown('m')
+        pyautogui.keyUp('command')
+        pyautogui.keyUp('shift')
+        pyautogui.keyUp('m')
+        key_pressed = None
+    
+
 def on_release(key):
     print(key)
+    key_pressed = None
     try:
-        if key.char == ('p'):
-            print("dupa")
-            pyautogui.keyDown('command')
-            pyautogui.keyDown('shift')
-            pyautogui.keyDown('m')
-            pyautogui.keyUp('command')
-            pyautogui.keyUp('shift')
-            pyautogui.keyUp('m')
+        key_pressed = key.char
     except:
         print("No key")
+
+    if key_pressed == ('p'):
+        pyautogui.keyDown('command')
+        pyautogui.keyDown('shift')
+        pyautogui.keyDown('m')
+        pyautogui.keyUp('command')
+        pyautogui.keyUp('shift')
+        pyautogui.keyUp('m')
+        key_pressed = None
 
 # Collect events until released
 with keyboard.Listener(
